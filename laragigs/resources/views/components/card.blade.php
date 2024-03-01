ac679f49f28a1b793c2cfe962a3a6780916dbe4c
LonG <bunlongly03@gmail.com>
2024-03-01 18:50:53 +0700

    Initial commit

---
+++ laragigs/resources/views/components/card.blade.php
@@ -0,0 +1,3 @@
+<div class="border border-gray-200 rounded p-6 {{ $attributes['class'] }}">
+    {{$slot}}
+</div>
---
+++ laragigs/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+
+//Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
