211b19345938a0934e5d9176f5ed501109998f74
LonG <bunlongly03@gmail.com>
2024-03-03 16:15:59 +0700

    Update Pagination

--- laragigs/app/Http/Controllers/ListingController.php
+++ laragigs/app/Http/Controllers/ListingController.php
@@ -13,7 +13,8 @@ class ListingController extends Controller
     //show all listings
     public function index(){
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(2)
+            
         ]);
     }
     
