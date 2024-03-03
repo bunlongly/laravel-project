9c8de081328f69ebcaf2376ce283e32856383af3
LonG <bunlongly03@gmail.com>
2024-03-03 12:04:39 +0700

    Validation & Store Listing

--- laragigs/app/Http/Controllers/ListingController.php
+++ laragigs/app/Http/Controllers/ListingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Unique;
 
 class ListingController extends Controller
 {
@@ -25,4 +27,20 @@ class ListingController extends Controller
         return view('listings.create');
     }
 
+    //Store Listing Data
+    public function store(Request $request) {
+      $formFields = $request->validate([
+        'title' => 'required',
+        'company' => ['required', Rule::Unique('listings', 'company')],
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email'],
+        'tags' => 'required',
+        'description' => 'required'
+      ]);
+
+      Listing::create($formFields);
+
+      return redirect('/');
+    }
 }
