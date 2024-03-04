7b178148fa2359956ff032a80a8de4e75e6d114f
LonG <bunlongly03@gmail.com>
2024-03-04 18:03:50 +0700

    Update User Login

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -55,4 +55,9 @@ Route::post('/users', [UserController::class, 'store']);
 Route::post('/logout', [UserController::class, 'logout']);
  
 
+//Show Login Form
+Route::get('/login', [UserController::class, 'login']);
 
+
+//Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
