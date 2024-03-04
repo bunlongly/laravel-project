71c0a8338afbe38e769e2e768e7a97f958d44ad4
LonG <bunlongly03@gmail.com>
2024-03-04 11:48:20 +0700

    Edit Listing

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -29,7 +29,11 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 Route::post('/listings', [ListingController::class, 'store']);
 
 
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+//Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
 
 
